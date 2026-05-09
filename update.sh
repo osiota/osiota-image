43c7f9bc2835e313e8ec74a0c648509bde589402
Simon Walz <contact@simonwalz.de>
2026-05-09 16:13:16 +0200

    ci: added update script

--- update.sh
+++ update.sh
@@ -4,17 +4,30 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "${SCRIPT_DIR}" || exit 127
 
-
-OSIOTA_VERSION="$(jq -r .version ../package.json)"
-
-PACKAGE_NEW="$(jq \
-  --arg version "${OSIOTA_VERSION}" \
-  '.dependencies.osiota = $version' \
-  "src/package.json"
-)"
-
-echo "${PACKAGE_NEW}" > "src/package.json"
-
 cd src/ || exit 127
 
-npm install
+npm-check-updates -u
+npm audit fix
+
+LAST_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+if [ -z "$LAST_TAG" ]; then
+  echo "No previous tags, proceeding with publish..."
+elif git diff --quiet "$LAST_TAG"; then
+  echo "No changes in $name since last tag ($LAST_TAG), skipping publish..."
+  exit 0
+fi
+
+npm ci --ignore-scripts || exit 1
+
+cd - || exit 127
+
+git add .
+git commit -m "chore: update dependencies"
+git push
+
+OSIOTA_VERSION="$(jq -r .dependencies.osiota src/package.json)"
+
+if ! git tag -v "${OSIOTA_VERSION}" >/dev/null 2>&1; then
+  git tag "$OSIOTA_VERSION}"
+  git push --tags
+fi
